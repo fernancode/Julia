9825288bb972688637f482403d58ce29ae7df3df
Fernando <FernandoAdelaFuente@gmail.com>
2020-06-06 20:35:04 -0700

    added comments

--- plotting.jl
+++ plotting.jl
@@ -1,2 +1,14 @@
 using Plots
-x = LinRange(0,2*pi,100)
+# to install the package, in julia i had to run "julia> using Pkg", "julia>Pkg.add("Pkgnames")
+
+x = LinRange(-pi, pi, 100)
+
+#need the dot operator to get the sin of the entire array
+y = sin.(x)
+
+#print the imaginary component of n
+n = 0 +2im
+println(imag(n))
+
+#plot x versus y
+plot(x,y)
