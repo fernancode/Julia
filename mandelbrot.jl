342917a13c90b487efd799c2d7e6d07b37fad328
Fernando <FernandoAdelaFuente@gmail.com>
2020-06-23 20:41:58 -0700

    it works! trying to implement multithreading now, issues with race condition.

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -1,18 +1,16 @@
 module MandelBrot
-
+using Plots
 #make some functions for mandelbrot stuff
 #find out if a number is part of the set
 #remember the mandelbrot is symmetrical about the real number plane
 
 function mandel(c)
     #determine if a number is in the set or not in the set - 
-    const max_iter = 1000
-    const bound = 2
+    max_iter = 1000;
+    bound = 2
     z = 0
     n = 0
     #if the magnitude of z exceeds two we know we are done.
-
-    #TODO:MAKE a break that breaks the loop if any values in the caluclation REPEAT,meaning PART OF THE SET.
     while abs(z)<bound && n<max_iter
         z = z^2+c
         n+=1
@@ -22,36 +20,51 @@ end
 
 #map n to a color
 function brot(n)
-    const rgb = 250
+    rgb = 250
     m = (n%rgb) /rgb#divide 250
     if 0< n <= 250
-        c = (1,m,0)
+        c = RGB(1,m,0)
     elseif 250<n<=500
-        c = (1-m,1,0)
+        c = RGB(1-m,1,0)
     elseif 500<n<=750
-        c = (0,1,m)
+        c = RGB(0,1,m)
     elseif 750<n<=999
-        c = (0,1-m,1)
+        c = RGB(0,1-m,1)
     else
-        c=(0,0,0)
+        c=RGB(0,0,0)
     end
     return c
+    #TODO: append this c to an array of color values
 end
 #mrandom
+
 
 function mandelbrot(reals,imags)
     #generate #real amount of points between -2 and 1
     #and #imag amount of points between 0 and i
     #determine if any of those combinations are in the mandelbrot set
     r = LinRange(-2,1,reals)
-    i = Linrange(0,1,imags)
-        for real_num in r
+    i = LinRange(0,1,imags)
+    master_list = zeros(Complex{Float64},reals*imags,1)
+    color_assign = Array{RGB{Float64}}(undef,reals*imags,1)
+    #n = Threads.Atomic{Int64}(1)
+    n = 1
+        Threads.@threads for real_num in r
             for imaginary_num in i
-                z = complex(real_num, imaginary_num)
-                n = mandel(z)
-                color = brot(n)
-                #TODO: actually, dont do it like this - need to make a matrix with all values first for sped + memory               
-                #TODO: plot real part of z and imaginary part of z with color n
+                #z = complex(real_num, imaginary_num) #create the number
+                #master_list[n] = z                      #add it to the list
+                #color_assign[n,1] = (brot ∘ mandel)(z) #function of function! \circ + tab
+
+                #or would this be faster? since we dont change z all the time?
+                master_list[n] = complex(real_num, imaginary_num)
+                color_assign[n,1] = (brot ∘ mandel)(complex(real_num, imaginary_num))
+                n+=1
+                #Threads.atomic_add!(n,1)
             end
         end
+        gr(markerstrokewidth=0,markerstrokealpha=0,markersize=1,legend=false)
+        scatter(master_list,markerstrokecolor=color_assign,color=color_assign,aspect_ratio=:equal)
     end
+
+#end statement for the module
+end
--- plotting.jl
+++ plotting.jl
@@ -1,14 +1,18 @@
 using Plots
 # to install the package, in julia i had to run "julia> using Pkg", "julia>Pkg.add("Pkgnames")
 
-x = LinRange(-pi, pi, 100)
+x = LinRange(-pi, pi, 10)
 
 #need the dot operator to get the sin of the entire array
-y = sin.(x)
+y = sin.(x)*im
 
-#print the imaginary component of n
-n = 0 +2im
-println(imag(n))
+a = RGB{Float64}(0,1,0)
+b = RGB{Float64}(1,0,0)
+c = RGB{Float64}(0,0,1)
+z = Array{RGB{Float64}}(undef,3,1)
+z[1] = a
+z[2] = b
+z[3] = c
 
-#plot x versus y
-plot(x,y)
+#scatter(y,markersize=5,markerstrokewidth=0,color= RGB(0,1,0))
+scatter(y,markersize=5,markerstrokewidth=0,markerstrokecolor = Colors.RGBA(0, 0, 0, 0),color=z,aspect_ratio=:equal)
